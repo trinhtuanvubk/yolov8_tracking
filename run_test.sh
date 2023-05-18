56a0b26d321d2bba596c0426f4f62859fdb29fe2
trinhtuanvu <vutuantrinh2000@gmail.com>
2023-05-18 10:53:55 +0700

    x

---
+++ run_test.sh
@@ -0,0 +1,3 @@
+python track.py --yolo-model model_repository/yolov8n.pt --tracking-method bytetrack --source test_images/pagi_11112021_1fps_19_jpg.rf.cddba45749cf6d29389263e7ace0de3f.jpg
+
+# python track.py --yolo-model model_repository/yolov8n.pt --tracking-method bytetrack --source https://www.youtube.com/watch?v=bHWgc5MPnPA
